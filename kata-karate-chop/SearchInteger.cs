5f3eb8864d08587a0387f2a8e7326ee74ef065ba
Ralph <ralphquidet@gmail.com>
2020-10-27 14:23:50 +0100

    four tests

--- kata-karate-chop/SearchInteger.cs
+++ kata-karate-chop/SearchInteger.cs
@@ -4,9 +4,19 @@ namespace kata_karate_chop
 {
     public class SearchInteger
     { 
-        public int chop(int target, Array hayStack)
+        public int chop(int target, int[] hayStack)
         {
-            return -1;
+            if (isEmpty(hayStack))
+            {
+                return -1;
+            }
+
+            return Array.FindIndex(hayStack, element => element == target);
+        }
+
+        private bool isEmpty(int[] hayStack)
+        {
+            return hayStack == null || hayStack.Length == 0;
         }
     }
 }
--- kata-karate-chop/SearchTests.cs
+++ kata-karate-chop/SearchTests.cs
@@ -9,9 +9,42 @@ namespace kata_karate_chop
         public void SearchInteger_EmptyArray_Minus1()
         {
             var searchInteger = new SearchInteger();
-            int[] sortedNumbers = {};
+            int[] sortedNumbers = { };
 
             var result = searchInteger.chop(1, sortedNumbers);
+
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void SearchInteger_ArrayContainsTargetInteger_Zero()
+        {
+            var searchInteger = new SearchInteger();
+            int[] sortedNumbers = { 1 };
+
+            var result = searchInteger.chop(1, sortedNumbers);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void SearchInteger_ArrayContainsMorThanTargetInteger_Zero()
+        {
+            var searchInteger = new SearchInteger();
+            int[] sortedNumbers = { 1, 2 };
+
+            var result = searchInteger.chop(2, sortedNumbers);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void SearchInteger_ArrayDoesnNotContainTargetInteger_Minus1()
+        {
+            var searchInteger = new SearchInteger();
+            int[] sortedNumbers = { 1, 2 };
+
+            var result = searchInteger.chop(3, sortedNumbers);
 
             Assert.Equal(-1, result);
         }
