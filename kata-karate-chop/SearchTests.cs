6283663f47a9617c8632b850cf05946c0bd50bc2
Ralph <ralphquidet@gmail.com>
2020-10-27 14:39:04 +0100

    Refactors tests

--- kata-karate-chop/SearchTests.cs
+++ kata-karate-chop/SearchTests.cs
@@ -16,35 +16,34 @@ namespace kata_karate_chop
             Assert.Equal(-1, result);
         }
 
-        [Fact]
-        public void SearchInteger_ArrayContainsTargetInteger_Zero()
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(3, 1)]
+        [InlineData(7, 2)]
+        [InlineData(9, 3)]
+        [InlineData(15, 4)]
+        public void SearchTargetInteger_ArrayDoesContainTargetInteger_Index(int target, int expectedIndex)
         {
             var searchInteger = new SearchInteger();
-            int[] sortedNumbers = { 1 };
+            int[] sortedNumbers = { 1, 3, 7, 9, 15 };
 
-            var result = searchInteger.chop(1, sortedNumbers);
+            var result = searchInteger.chop(target, sortedNumbers);
 
-            Assert.Equal(0, result);
+            Assert.Equal(expectedIndex, result);
         }
 
-        [Fact]
-        public void SearchInteger_ArrayContainsMorThanTargetInteger_Zero()
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        public void SearchTargetInteger_ArrayDoesNotContainTargetInteger_Minus1(int target)
         {
             var searchInteger = new SearchInteger();
-            int[] sortedNumbers = { 1, 2 };
+            int[] sortedNumbers = { 1, 3, 7, 9, 15 };
 
-            var result = searchInteger.chop(2, sortedNumbers);
-
-            Assert.Equal(1, result);
-        }
-
-        [Fact]
-        public void SearchInteger_ArrayDoesnNotContainTargetInteger_Minus1()
-        {
-            var searchInteger = new SearchInteger();
-            int[] sortedNumbers = { 1, 2 };
-
-            var result = searchInteger.chop(3, sortedNumbers);
+            var result = searchInteger.chop(target, sortedNumbers);
 
             Assert.Equal(-1, result);
         }
